59dd22d0d4bb7c963429078ae490f2e4cbc29436
Aiman Yoviar Maulana <145090629+viar15@users.noreply.github.com>
2023-10-18 11:18:19 +0700

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Data{
+    int siswa_id;
+    int uts_score;
+    int uas_score;
+};
+
+int main()
+{
+    int n, avg = 0;
+    cout << "Masukan jumlah mahasiswa : ";
+    cin >> n;
+    Data mahasiswa[n];
+    
+    for(int i = 0; i < n; i++){
+        cout << "Masukan ID Mahasiswa : ";
+        cin >> mahasiswa[i].siswa_id;
+        cout << "Masukan nilai uts Mahasiswa : ";
+        cin >> mahasiswa[i].uts_score;
+        cout << "Masukan nilai uas Mahasiswa : ";
+        cin >> mahasiswa[i].uas_score;
+    }
+    
+    for(int i = 0; i < n; i++){
+        cout << "ID Mahasiswa: "<< mahasiswa[i].siswa_id << endl;
+        avg = mahasiswa[i].uts_score + mahasiswa[i].uas_score;
+        cout << "Rata-Rata: " << avg / 2 << endl;
+    }
+    
+
+    return 0;
+}
